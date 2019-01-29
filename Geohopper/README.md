de7225d5887491c7883a993a98390a9cd67fb7e3
Brian Wilson <wilson@bubba.org>
2019-01-28 20:02:14 -0500

    initial commit

---
+++ Geohopper/README.md
@@ -0,0 +1,63 @@
+GeoHopper Multi-User Virtual Presence Updater for Hubitat
+=======
+<br>
+GeoHopper is an app for iOS and Android that uses either device GPS or
+Bluetooth LE to GeoHopper
+[iBeacons](http://store.twocanoes.com/collections/ibeacons). This device
+driver and SmartApp will allow you to use Bluetooth on your mobile
+device to set presence in SmartThings. This means you can now get accurate
+presence without using GPS and without using a presence sensor.
+
+Requirements
+------------
+To get started you'll need:
+- [GeoHopper](https://itunes.apple.com/us/app/geohopper/id605160102?mt=8).
+- An iBeacon (or you can use GPS in the GeoHopper). 
+- Hubitat Hub
+	- [A Virtual Presence Device](https://github.com/ajpri/STApps/blob/master/devicetypes/ajpri/virtual-mobile-presence.src/virtual-mobile-presence.groovy)
+	- A Hubitat app assigned to your Virtual Presence Device
+
+Installation
+--------------------
+1. Go to Drivers Code and create a new device using [virtual-mobile-presence.groovy](https://github.com/ajpri/STApps/blob/master/devicetypes/ajpri/virtual-mobile-presence.src/virtual-mobile-presence.groovy).
+2. Go to Devices and create a new virtual device '''name it whatever the location name will be called in
+GeoHopper with a - and a USER after it'''.  Network Device ID can be '''VIRTUAL_PRESENCE_1'''. Type will be
+the type you configured in step 1.  Make sure Location and/or Hub is selected
+and select create. For intance, mine is named "Virtual Presence - Brian" and my
+Geohopper location is named "Virtual Presence". You can create multiple
+virtual devices and give them different names after the "-" then you can
+reference them after the location below in #5 and use that new webhook in #8.
+3. Go to Apps Code and
+create a new App from code geohopper-presence.groovy. Save it. Click Oauth.
+4. Grab the API Token and Endpoint URL from the install window.
+5. Copy the Endpoint URL and add '''/location/?access_token=[API Token From
+Above]'''.  This will be your URL to configure in GeoHopper.  It should look 
+something like this:
+http://192.168.1.xx/apps/api/xx/location/[your user]?access_token=adafae03-0330-4aeb-b15e-xxxxxxxx
+or https://cloud.hubitat.com/api/xxx-xxx-xx-xxx/apps/xx/location/[your user]?access_token=adafae03-0330-4
+aeb-b15e-xxxxxxxxx.  For the example in #2 above, [your user] is Brian.  
+6. Now paste this URL into your browser and make sure you get the following
+response:
+<code>
+{"error":true,"type":"SmartAppException","message":"Yep, this is the right URL."}
+</code>
+7. Install GeoHopper app and your iBeacon.  In the app, go to Settings->Web
+Service and configure a new web service using the URL above and 
+select "Post". You can
+select Execute Locally if the server will be on the same network your mobile
+device is on when the triggers need to run. 
+8. Configure a GeoHopper location or rename your iBeacon to be whatever the
+name of your device was in step 2 (minus the "- Name" part since that will get
+passed on your webhook URL).  For this location, enable your web service
+and perform a test.  It should perform the "On" or "Present" action (check your
+ Hubitat device to make sure you see the logs rolling in.  If not, make sure
+you have your URL correct in the GeoHopper app. Make sure step 7 completed
+correctly.
+9. You should now have a virtual presence sensor that you can tie to
+Hubitat actions. 
+
+Bugs/Contact Info
+-----------------
+Bug me on Twitter at [@brianwilson](http://twitter.com/brianwilson) or email me [here](http://cronological.com/comment.php?ref=bubba).
+
+
---
+++ Geohopper/geohopper-presence.groovy
@@ -0,0 +1,143 @@
+/**
+ *  Geohopper API Presence App
+ *
+ *  Copyright 2015 Brian Wilson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Geohopper Multi-User API Presence App",
+    namespace: "bw",
+    author: "Brian Wilson",
+    description: "Geohopper API Presence App",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    oauth: true)
+
+
+preferences {
+  page(name: "setupScreen")
+}
+
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+def setupScreen(){
+    if(!state.accessToken){	
+        //enable OAuth in the app settings or this call will fail
+        createAccessToken()	
+    }
+    def uri = getFullLocalApiServerUrl() + "/?access_token=${state.accessToken}"
+	def extUri = fullApiServerUrl() + "/?access_token=${state.accessToken}"
+    return dynamicPage(name: "setupScreen", uninstall: true, install: true){
+		section ("Allow external service to control these things...") {
+    		input "presence", "capability.presenceSensor", multiple: true, required: true
+    	}
+        section(){ 
+            paragraph("Use the following URI to access the page: <a href='${uri}'>${uri}</a> or <a href='${extUri}'>${extUri}</a> for external access.")
+        }
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+def listLocations() {
+    def resp = []
+    presence.each {
+      log.debug "RECEIVED: ${it.displayName}, attribute ${it.name}, ID: ${it.id}"
+      resp << [Name: it.displayName, ID: it.id]
+    }
+    return resp
+}
+
+def deviceHandler(evt) {}
+
+void correctURL () {
+	 httpError(200, "Yep, this is the right URL.")
+}
+
+void validCommands() {
+	 httpError(200, "Valid Commands: /listLocations, /location/<user>")
+}
+
+void updateLocation() {
+    update(presence)
+}
+
+private void update (devices) {
+   
+	// worked on ST :(
+	//def data = request.JSON
+    //def location = data.location
+    //def event = data.event
+   	def jsonSlurper = new JsonSlurper()
+    def plexJSON = params.findAll { key,value -> key =~ /location/ } 
+	def firstKey = plexJSON.keySet().stream().findFirst().get();
+	def json = jsonSlurper.parseText(firstKey)
+	log.debug "json: ${json.location}"
+	log.debug "json event ${json.event}"
+	def location = json.location
+   	def event = json.event
+   	def user = params.user
+   	def deviceName = location + "-" + user
+   	def device = devices.find { it.displayName == deviceName }
+   	log.debug "update, request: params: ${params}, data, ${data}, devices: $devices.id, $devices.displayName"
+   	log.debug "event: ${event} device: ${device} location: ${location} user: ${user} deviceName: ${deviceName}"
+   
+   	if (location){
+        if (!device) {
+        	log.debug "Error: device (${device}) not found"
+            httpError(404, "Device not found")
+     	} else {
+            if(event == "LocationExit"){
+            	log.debug "Turning device off"
+                device.off();
+            } else { 
+            	log.debug "Turning device on"
+                device.on();
+            }
+        }
+    }
+}
+
+mappings {
+	path("/") {
+		action: [
+			GET: "validCommands"
+		]
+	}
+    path("/listLocations") {
+        action: [
+            GET: "listLocations"
+        ]
+    }
+    path("/location/:user") {
+    	action: [
+            POST: "updateLocation",
+            GET: "correctURL"
+        ]
+    }
+}
