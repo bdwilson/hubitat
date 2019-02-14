d3b6741c6d999165811ff4582889f42f47a86d31
Brian Wilson <wilson@bubba.org>
2019-02-13 20:24:48 -0500

    Added initial version of Honeywell TCC IFTTT driver.

---
+++ HoneywellTCC/HoneywellTCC-IFTTT-driver.groovy
@@ -0,0 +1,114 @@
+/**
+* IFTTT Thermostat Driver for Hubitat
+*
+* v1.0
+*/
+metadata {
+    definition (name: "Honeywell IFTTT Thermostat Driver", namespace: "brianwilson-hubitat", author: "Brian Wilson") {
+        capability "Thermostat"
+        command    "setFollowSchedule"
+        attribute  "followSchedule",     "string"
+    }
+
+    main "temperature"
+    details(["thermostatFanMode", "heatingSetpoint", "coolingSetpoint","fanOperatingState","followSchedule","status"])
+
+    preferences {
+        input("APIKEY", "text", title: "IFTTT Webhook API Key", description: "Your IFTTT Webhook API Key", required: true)
+        input("deviceName", "text", title: "Device Name/Location:",required: true, defaultValue: "downstairs")
+        input("setHeatPerm", "text", title: "Set Heat Name (perm)", required: true, defaultValue: "set_heat_perm")
+        input ("setCoolPerm", "text", title: "Set Cool Name (perm)", required: true, defaultValue: "set_cool_perm")
+        input ("resume", "text", title: "Resume Name", required: true, defaultValue: "resume")
+        input ("setCool", "text", title: "Set Cool Perm Hold Name (hold hours below)", required: true, defaultValue: "set_cool")
+		input ("setHeat", "text", title: "Set Heat Perm Hold Name (hold hours below)", required: true, defaultValue: "set_heat")
+		input ("holdTime", "number", title: "Hold # Hours: (0 for perm Hold)", required: true, defaultValue: "0")
+		//input ("tempHold", "text", title: "Temp Hold Name", required: false, defaultValue: "temp_hold")
+	    input ("fanModeOn", "text", title: "Fan Mode On Name", required: false, defaultValue: "fan_mode_on")
+	    input ("fanModeAuto", "text", title: "Fan Mode Auto Name", required: false, defaultValue: "fan_mode_auto")
+		//input name: "permHold", type: "bool", title: "Enable debug logging?", defaultValue: true
+        input name: "debugOutput", type: "bool", title: "Enable debug logging?", defaultValue: true
+    }
+}
+
+
+// parse events into attributes
+def parse(String description) {
+	logDebug("Here: ${description}")
+}
+
+def ifttt_call (Map map = [:], String cmd, String event) {
+	def key = settings.deviceName + "-" + cmd
+	def ifttt_base = "https://maker.ifttt.com/trigger/" + key + "/with/key/" + settings.APIKEY
+
+	if (map.parm1) {
+		ifttt_base = ifttt_base + "?value1=" + map.parm1
+	}
+	
+	if (map.parm2) {
+		ifttt_base = ifttt_base + "&value2=" + map.parm2
+	}
+	
+	logDebug("Sending request (${event}): ${ifttt_base} (i.e. IFTTT will send an event, even if it doesn't exist. So check your names if you're having issues)")
+	
+	try {
+		httpGet(uri: "${ifttt_base}"){response ->
+        if(response.status != 200) {
+            log.error "ERROR: ${response.status}"
+		} else {
+			logDebug("Response: ${response.data}")
+			sendEvent(name: "${event}", value: "${map.parm1}")
+        }
+   	}   
+	}
+	catch (groovyx.net.http.HttpResponseException hre) {
+                    if (hre.getResponse().getStatus() != 200) {
+						log.error "ERROR: ${hre.getResponse().getStatus()} ${hre.getResponse().getData()}"
+						log.error "Make sure you have created a webhook called \"${key}\" in IFTTT."
+						log.error "Make sure you select the proper options (heat or cool - where applicable)"
+						if ((map.parm1) || (map.parm2)) {
+							log.error "Also make sure you configure Value1 and Value2 variables for optional input items where applicable."
+						}
+                    }
+    }
+}
+	
+def setHeatingSetpoint(Double temp)
+{
+	logDebug("Settings: ${settings.setHeat}")
+	if ((settings.setHeat) && (settings.holdTime)) {
+		ifttt_call(settings.setHeat,"heatingSetpoint",parm1:temp,parm2:settings.holdTime)  	
+	} else {
+		ifttt_call(settings.setHeatPerm,"heatingSetpoint",parm1:temp)  	
+	}
+}
+
+def setFollowSchedule() {
+	logDebug("Settings: ${settings.resume}")
+	ifttt_call(settings.resume,"followSchedule") 
+}
+
+
+def setCoolingSetpoint(double temp) {
+	logDebug("Settings: ${settings.setCool}")
+	if ((settings.setCool) && (settings.holdTime > 0)) {
+		ifttt_call(settings.setCool,"coolingSetpoint",parm1:temp,parm2:settings.holdTime)  	
+	} else {
+		ifttt_call(settings.setCoolPerm,"coolingSetpoint",parm1:temp)  	
+	}
+}
+
+def fanOn() {	
+	logDebug("Settings: ${settings.fanModeOn}")
+	ifttt_call(settings.fanModeOn,"fanOperatingState",parm1:'on')  	
+}
+
+def fanAuto() {
+	logDebug("Settings: ${settings.fanModeAuto}")
+	ifttt_call(settings.fanModeAuto,"fanOperatingState",parm1:'auto')  	
+}
+
+private logDebug(msg) {
+	if (settings?.debugOutput || settings?.debugOutput == null) {
+		log.debug "$msg"
+	}
+}
---
+++ HoneywellTCC/README.md
@@ -0,0 +1,75 @@
+# Honeywell TCC IFTTT Driver
+
+This is a Driver for Hubitat that supports controlling a Honeywell Total
+Connect Comfort (TCC) Thermostat via IFTTT. Honeywell wants to be mean and not
+support integrations with other home hubs, but they will integrate with IFTTT.
+Thankfully, IFTTT supports webhooks so users can make their own calls. This
+driver uses (abuses?) the ability to use webhooks to control devices which
+you've integrated. This specific example works for my Honeywell Prestige
+Thermostat, but it may work for others.
+
+# Limitations
+
+You're not going to get all thermostat functions. You won't be able to see what
+the temp is, but perhaps you have another sensor on your Hubitat hub that will
+give you that. All I really wanted to do was adjust temps, do permanent and
+temporary holds, and then resume the schedule - so that's all this does.
+
+Commands Supported:
+setHeatingSetPoint (permanent or for X hours)
+setCoolingSetPoint (permanent or for X hours)
+setFollowSchedule
+fanOn
+fanAuto
+
+So you'll essentially have to (well, you have to create the ones you want to
+use) create 7 webhooks per thermostat you wish to control. 
+
+# Installation
+
+1) [IFTTT Account](https://ifttt.com). You need to connect your Honeywell
+services to your account. Grab your API key from
+[here](https://ifttt.com/services/maker_webhooks/settings) - it's the part
+after the ''/use/'' in the URL.
+2) Create a ''New Applet''. For ''This'' select Webhook. Click on the box ''Receive a web
+request''.
+3) For your event name, you'll make it be ''device''-''command''.  For
+instance, I have two devices - upstairs and downstairs, and we have 7 commands
+per device. So the first webhook I create will be ''downstairs-setCoolPerm''
+4) For ''That'' select ''Honeywell Total Connect Comfort''. 
+5) Then select the function you're creating - for this example, I'd select
+''Set temperature to a permanent hold''. Then select the options for your
+thermostat (which thermostat, cool or heat). For ''Target Temperature'', delete the field completely and click
+on ''Add Ingredient'' and select ''Value1''. Then select ''Celsius'' or
+''Fahrenheit''.  For your recipies that need two variables (the hold for
+certain time), set ''Value2'' to be that variable. 
+6) Click ''Create Action''. 
+
+Optional: Enable notifications to verify things are working, but you can just
+use the TCC app or website or thermstat to verify, because you'll eventually
+turn them off.
+
+Your events should look like this:
+!(https://github.com/bdwilson/bdwilson.github.io/blob/master/images/HoneywellTCC.png)
+
+Next, install your driver.
+
+1) Go to the raw version of the .groovy code on Github.
+2) Go to Hubitat -> Drivers Code -> and paste the code in from above.
+3) Go to Hubitat -> Devices -> Add Virtual Device
+4) Scroll to the bottom of the list and select ''Honeywell TCC IFTTT Thermostat Driver''
+5) Configure your command names to match what you configured above at IFTTT. It
+should look similar to this:
+!(https://github.com/bdwilson/bdwilson.github.io/blob/master/images/HoneywellTCC2.png)
+6) If you want to do perm holds unstead of holds for a # of hours, adjust your
+''Hold#'' number, otherwise, all your changes will be permanent until you do a
+''setFollowSchedule''.
+7) Check the logs to see if things are working, if so, turn off debug.
+Remember, not everything works just what's listed above at the top of the page.
+
+# Questions/Comments/Pull Requests
+Bug me on Twitter at [@brianwilson](http://twitter.com/brianwilson) or email me
+[here](http://cronological.com/comment.php?ref=bubba) or on the [Hubitat
+community](https://community.hubitat.com/)/@brianwilson.
+
+
