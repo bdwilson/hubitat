cd0223e7b4d07f8c923cb0cdcaa494caac7fb3b9
Brian Wilson <wilson@bubba.org>
2020-04-16 21:06:24 -0400

    limit number of updates if a bunch of lights change status at the same/similar time

--- Arduino/SmarterBulbs.groovy
+++ Arduino/SmarterBulbs.groovy
@@ -1,9 +1,10 @@
 /**
- *  Smarter Bulbs - https://raw.githubusercontent.com/bdwilson/hubitat/master/Arduino/SmarterBulbs.groovy
+ *  Smarter Bulbs - https://raw.githubusercontent.com/bdwilson/hubitat/master/Arduino/SmarterBulbs.groovy 
  *
  *  Copyright 2016 Nick Sweet.
- *  Updates by Brian Wilson for Hubitat
+ *  Updates by Brian Wilson
  *  - only update status on init or if change (no scheduled job) - 02/20/2020
+ *  - only do one update if there's a series of light changes (unschedule/schedule) - 4/16
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -25,8 +26,8 @@ definition(
 	author: "nick@sweet-stuff.cc",
 	description: "Save the state of a bunch of bulbs and reset when 'Canary' bulb turns on",
 	category: "My Apps",
+    importUrl: "https://raw.githubusercontent.com/bdwilson/hubitat/master/Arduino/SmarterBulbs.groovy",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/App-LightUpMyWorld.png",
-    importUrl: "https://raw.githubusercontent.com/bdwilson/hubitat/master/Arduino/SmarterBulbs.groovy", 
 	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/App-LightUpMyWorld@2x.png"
 )
 preferences {
@@ -36,35 +37,44 @@ preferences {
     section("Zigbee bulbs to monitor") {    
         input "slaves","capability.switch", multiple: true
     }
+    section("Logs") {
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    }
+}
+
+def logsOff() {
+    log.warn "debug logging disabled..."
+    app.updateSetting("logEnable", [value: "false", type: "bool"])
 }
 
 def installed() {
-    log.debug "Installed with settings: ${settings}"
+    log.debug "SmarterBulbs: Installed with settings: ${settings}"
     initialize()
 }
 
 def updated() {
-    log.debug "Updated with settings: ${settings}"
+    log.debug "SmarterBulbs: Updated with settings: ${settings}"
     unsubscribe()
     initialize()
 }
 
 def initialize() {
+    log.warn "SmarterBulbbs: Debug logging is: ${logEnable == true}"
+    if (logEnable) runIn(3600, logsOff)
     subscribe(slaves, "switch", saveStates)
     subscribe(canary,"switch.on", checkRestore)
-    //canary.refresh() // no need to refresh canary as we'll do this via ESP12 
-    initStates()
-
-    
+    checkStates()
 }
 
-def initStates() {
+def checkStates() {
+    canary.refresh()
     def lightsOff = [:]
     slaves?.each {
-			if (it.currentSwitch == "off"){
+			//if (it.currentSwitch == "off"){
         	    //log.debug "${it.id} value ${it.currentSwitch}" 
-        	    lightsOff[it.id]="off"
-        	}
+                if (logEnable) log.debug "SmarterBulbs: Saving State for ${it.displayName} (${it.id}) = ${it.currentSwitch}" 
+        	    lightsOff[it.id]=it.currentSwitch
+        	//}
 	}
    	state.lOff = lightsOff   
 }
@@ -72,24 +82,33 @@ def initStates() {
        
 def saveStates(evt) {
     if (canary.currentSwitch == "off") {  // only keep track of state if canary is off
-        state.lOff[evt?.deviceId]=evt.value
-        //log.debug "Saving State for ${evt.displayName} (${evt?.deviceId}) = ${evt.value}" 
+            unschedule(checkStates) // unschedule + runIn combo keeps you from having 
+            runInMillis(5000, checkStates)  // each light re-checked if 4 or 5 lights go off at once
+        //state.lOff[evt?.deviceId]=evt.value
+        //log.debug "SmarterBulbs: Saving State for ${evt.displayName} (${evt?.deviceId}) = ${evt.value}" 
+    } else {
+       log.info "SmarterBulbs: Found canary bulb on while saving states; doing a restore."
+       unschedule(checkRestore)
+       runInMillis(5000, checkRestore)
+       //checkRestore()
     }
 }
 
 def checkRestore(evt) {
     if ("on" == canary.currentSwitch) { 
-    	log.info "Canary came on; restoring off states."
+    	log.info "SmarterBulbs: Canary came on; restoring off states."
         restoreState()
         pauseExecution(200)
+        log.info "SmarterBulbs: Turning off canary"
         canary.off()
-        }
+    } 
 }
 
 private restoreState() {
   slaves?.each {
       if (state.lOff[it.id] == "off") { 
-    	log.info "Turning $it.label off based on previous state"
+    	log.info "SmarterBulbs: Turning $it.label off based on previous state"
+        //pauseExecution(200)
 		it.off()
     }
   }
