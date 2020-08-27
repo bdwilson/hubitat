1e83aad23fdc89e392e92f76388a7ad403999f13
Brian Wilson <wilson@bubba.org>
2020-08-26 21:08:32 -0400

    updates to allow multiple recipients, bcc option and high priority

--- Sendgrid/Sendgrid.groovy
+++ Sendgrid/Sendgrid.groovy
@@ -4,7 +4,9 @@
 *   Requirements: Requires at least a free SendGrid account & validated email address.
 *   Platform: Hubitat
 *   Modification History:
-
+*     26-AUG-2020 - Added options for multiple recipient addresses, option to BCC
+*                   recipients, option to send with HIGH priority.
+*
 *   Inspired by original work for SmartThings & Hubitat by: Dan Ogorchock for Pushover.
 *
 *  Copyright 2020 Brian Wilson
@@ -20,17 +22,20 @@
 *
 *
 */
+
 preferences {
-    input("apiKey", "text", title: "API Key:", description: "Sendgrid API key", required: true)
-    input("from", "text", title: "From Address:", description: "From Address", required: true)
-    input("to", "text", title: "To Address:", description: "To Address", required: true)
+    input("apiKey", "text", title: "SendGrid API Key:", description: "", required: true)
+    input("from", "text", title: "Sender Address:", description: "An approved address or domain configured within SendGrid", required: true)
+    input("to", "text", title: "Recipient Address(es):", description: "Separate multiple emails with a space", required: true)
     input("subject", "text", title: "Subject:", description: "", required: true)
+    input("urgent", "bool", title: "Send with high priority", description: "", required: true)
+    input("bcconly", "bool", title: "BCC recipient(s) - will show as sent to your FROM address", description: "", required: true)
     input("debug", "bool", title: "Enable Debug Logging", required: true, defaultValue: false)
 }
 
 metadata {
-    definition (name: "Sendgrid", namespace: "brianwilson-hubitat", author: "Brian Wilson", 
-			    importUrl: "https://raw.githubusercontent.com/bdwilson/hubitat/master/Sendgrid/Sendgrid.groovy") {
+    definition (name: "Sendgrid", namespace: "brianwilson-hubitat", author: "Brian Wilson",
+               importUrl: "https://raw.githubusercontent.com/bdwilson/hubitat/master/Sendgrid/Sendgrid.groovy") {
         capability "Notification"
         capability "Actuator"
         capability "Speech Synthesis"
@@ -55,9 +60,26 @@ def speak(message) {
 def deviceNotification(message) {
   
     // Define the initial postBody keys and values for all messages
+    def postBody = ""
+    def headers = ""
+    def bcc = ""
+    def values = settings.to.split(' ')
+    def newTo = ""
+    for (i in values) {
+        newTo += '{"email": "' + i + '"},'
+    }
+    newTo = newTo.substring(0, newTo.length()-1)
 
-    def postBody = '{"personalizations": [{"to": [{"email": "' + to + '"}]}],"from": {"email": "' + from + '"},"subject": "' + subject + '","content": [{"type": "text/plain", "value": "' + message + '"}]}'
-              
+    if (settings.urgent) {
+         headers = '"headers": {"Priority": "Urgent", "Importance": "high", "X-Priority": "1"},'
+    }
+    if (settings.bcconly) {
+        bcc = ',"bcc": [' + newTo + ']'
+        newTo = '{"email": "' + settings.from + '"}'
+    }
+         
+    postBody = '{"personalizations": [{"to": [' + newTo + ']' + bcc + '}],"from": {"email": "' + settings.from + '"},' + headers + '"subject": "' + settings.subject + '","content": [{"type": "text/plain", "value": "' + message + '"}]}'
+         
     ifDebug("Sending Message: $message [${postBody}]")
 
     // Prepare the package to be sent
@@ -69,20 +91,23 @@ def deviceNotification(message) {
         requestContentType: "application/x-www-form-urlencoded",
         body: postBody
     ]
-
-        httpPost(params){response ->
+    try {
+       httpPost(params){response ->
             if(response.status != 202) {
                 sendPush("ERROR: 'Sendgrid' received HTTP error ${response.status}. Check your API key.")
                 log.error "Received HTTP error ${response.status}. ${response}"
             }
             else {
-                ifDebug("Message queued for sending by Sendgrid Server")
-        }
+                ifDebug("Message queued for sending by Sendgrid Server.")
+       }
+      }
+    } catch (e) {
+        log.error "SendGrid: Error making httpPost: $e"
     }
 }
 
 private ifDebug(msg) {
     if (settings?.debug || settings?.debug == null) {
-        log.debug 'Sendgrid: ' + msg
+        log.debug 'SendGrid: ' + msg
     }
 }
