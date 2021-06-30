fa0475454145388d7afd04cfda8df613463b9831
Brian Wilson <wilson@bubba.org>
2021-06-30 16:25:15 -0400

    Update camect_connect-driver.groovy

--- Camect/camect_connect-driver.groovy
+++ Camect/camect_connect-driver.groovy
@@ -1,6 +1,6 @@
 /**
  *  Hubitat Device Handler: Camect Connect
- *  Version: 1.3.2
+ *  Version: 1.3.3
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
